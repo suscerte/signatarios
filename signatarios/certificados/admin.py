86d9fd4a2a0bb6e61ef6a99693a1786d0c9eb133
luis alfaro <lalfaro@lalfaro.suscerte.gob.ve>
2015-08-19 11:36:18 -0430

    subiendo certificados al admin completo

--- signatarios/certificados/admin.py
+++ signatarios/certificados/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from certificados.models import Certificado
+from certificados.models import Signatario
+from certificados.models import Estado
 
 admin.site.register(Certificado)
+admin.site.register(Signatario)
+admin.site.register(Estado)
