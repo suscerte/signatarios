3adc5013db7ea81a63e1bb5a1eaaf318b847a0f0
luis alfaro <lalfaro@lalfaro.suscerte.gob.ve>
2015-08-19 11:27:26 -0430

    agregando las aplicaciones al admin

--- signatarios/certificados/admin.py
+++ signatarios/certificados/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from certificados.models import Certificado
 
-# Register your models here.
+admin.site.register(Certificado)
--- signatarios/proveedores/admin.py
+++ signatarios/proveedores/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from proveedores.models import Proveedor
 
-# Register your models here.
+admin.site.register(Proveedor)
