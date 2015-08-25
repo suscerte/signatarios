e25209dd84b7bada935b7525da79b1c9ab415e52
Axel Díaz <diaz.axelio@gmail.com>
2015-08-25 11:32:07 -0430

    Añadiendo nombre plural de proveedores

--- signatarios/proveedores/models.py
+++ signatarios/proveedores/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Proveedor(models.Model):
         nombre = models.CharField(max_length = 1024)
 
+        class Meta:
+            verbose_name_plural = 'proveedores'
+
         def __unicode__(self):
                 return self.nombre
 
